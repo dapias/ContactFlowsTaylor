0764cd45fa2db05c0638499eb7285b7e3956dd9d
dapias <dandroveros@gmail.com>
2015-12-08 14:42:55 -0600

    Agregué nueva carpeta para analizar el oscilador armónico con un parámetro en f(S)

---
+++ ContactHamiltonian/steeper.jl
@@ -0,0 +1,40 @@
+using TaylorSeries
+
+
+const ordenTaylor = 28
+const epsAbs = 1.0e-20
+
+# Integrador
+function taylorStepper{T<:Number}(campo::Function, vec0::Array{T,1}, order::Int64, epsilon::T, c::Float64, beta::Float64)
+  #Esta función recibe un arreglo correspondiente a los datos iniciales, resuelve las ecuaciones de movimiento y evalúa la solución en el tiempo óptimo (determinado mediantela función stepsize).
+
+  n = length( vec0 )
+  vec1T = campo( vec0, c, beta)
+
+  # Step-size
+  hh = Inf
+  for i=1:n
+    h1 = stepsize( vec1T[i], epsilon )
+    hh = min( hh, h1 )
+  end
+
+  # Values at t0+h
+#   for i=1:n
+#     vec0[i] = evaluate( vec1T[i], hh )
+#   end
+
+  return hh, vec1T
+end
+
+
+# Returns the maximum step size from epsilon and the last two coefficients of the x-Taylor series
+function stepsize{T<:Real}(x::Taylor1{T}, epsilon::Float64)
+  ord = x.order
+  h = Inf
+  for k in [ord-1, ord]
+    kinv = 1.0/k
+    aux = abs( x.coeffs[k+1] )
+    h = min(h, (epsilon/aux)^kinv)
+  end
+  return h
+end
