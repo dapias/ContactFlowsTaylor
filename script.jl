77453a869bd83c4d94f9c976cd5d10fef4a7ed1d
dapias <dandroveros@gmail.com>
2015-12-19 13:36:45 -0600

    Script almost ready

--- script.jl
+++ script.jl
@@ -1,7 +1,18 @@
-include("src/integrator.jl")
+#include("src/integrator.jl")
+push!(LOAD_PATH,"src/")
 
 import YAML
 using ContactIntegrator
+using HDF5
+
+println("type the output filename:")
+name = string(readline(STDIN))
+filename = name[1:end-1]
+
+while isfile("./HDF5/$filename.hdf5")
+    println("The filename typed already exists in the HDF5 folder. Try another one:")
+    filename = string(readline(STDIN))
+end
 
 parameters = YAML.load(open("parameters.yaml"))
 
@@ -18,4 +29,25 @@ catch
     condinic = [rand(), rand(), rand()]
 end
 
-contactintegration(n, condinic, deltat, c, beta)
+file = h5open("./HDF5/$filename.hdf5", "w")
+
+attrs(file)["condinicial"] = condinic
+attrs(file)["nsampling"] = n
+attrs(file)["deltatsample"] = deltat
+attrs(file)["beta"] = beta
+attrs(file)["c"] = c
+
+results = contactintegration(n, condinic, deltat, c, beta)
+t = results[1]
+q = results[2]
+p = results[3]
+S = results[4]
+
+file["simulation-1/p"] = p
+file["simulation-1/q"] = q
+file["simulation-1/t"] = t
+file["simulation-1/S"] = S
+
+close(file)
+
+println("The simulation was succesfully done. \nOutput in /HDF5/$filename.hdf5")
