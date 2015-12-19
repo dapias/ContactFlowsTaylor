9e95a76aa0e62bc977d1681f40d3e9071163cac9
dapias <dandroveros@gmail.com>
2015-12-19 16:43:22 -0600

    Considered that the user needs more than one simulation. Adapted the code for that

--- script.jl
+++ script.jl
@@ -10,8 +10,8 @@ name = string(readline(STDIN))
 filename = name[1:end-1]
 
 while isfile("./HDF5/$filename.hdf5")
-    println("The filename typed already exists in the HDF5 folder. Try another one:")
-    filename = string(readline(STDIN))
+  println("The filename typed already exists in the HDF5 folder. Try another one:")
+  filename = string(readline(STDIN))
 end
 
 parameters = YAML.load(open("parameters.yaml"))
@@ -20,33 +20,48 @@ c = parameters["c"]
 beta = parameters["beta"]
 n = parameters["nsampling"]
 deltat = parameters["deltatsample"]
-condinic = Array{Float64}(3)
+nsimulations = parameters["nsimulations"]
+initcond = Array{Float64}(3)
 
-try 
-    condinic = parameters["condinic"]
-    condinic = [condinic["q0"], condinic["p0"], condinic["S0"]] 
-catch
-    condinic = [rand(), rand(), rand()]
+if nsimulations == 1
+  try
+    initcond = parameters["initcond"]
+    initcond = [initcond["q0"], initcond["p0"], initcond["S0"]]
+  catch
+    initcond = [rand(), rand(), rand()]
+  end
+else
+   initcond = [rand(), rand(), rand()]
 end
 
 file = h5open("./HDF5/$filename.hdf5", "w")
 
-attrs(file)["condinicial"] = condinic
 attrs(file)["nsampling"] = n
 attrs(file)["deltatsample"] = deltat
 attrs(file)["beta"] = beta
 attrs(file)["c"] = c
 
-results = contactintegration(n, condinic, deltat, c, beta)
-t = results[1]
-q = results[2]
-p = results[3]
-S = results[4]
+for i in 1:nsimulations
 
-file["simulation-1/p"] = p
-file["simulation-1/q"] = q
-file["simulation-1/t"] = t
-file["simulation-1/S"] = S
+  if i > 1
+    initcond = [rand(), rand(), rand()]
+  end
+
+  file["simulation-$i/initcond"] = initcond
+
+  results = contactintegration(n, initcond, deltat, c, beta)
+
+  t = results[1]
+  q = results[2]
+  p = results[3]
+  S = results[4]
+
+  file["simulation-$i/p"] = p
+  file["simulation-$i/q"] = q
+  file["simulation-$i/t"] = t
+  file["simulation-$i/S"] = S
+end
+
 
 close(file)
 
